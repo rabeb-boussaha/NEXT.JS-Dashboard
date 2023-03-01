fe91293078007aed7df204093b5718378f8a2b33
Boussaha Rabeb <rabeb-boussaha>
2023-03-01 11:58:05 +0100

    dashbord using nextjs and chartjs

---
+++ pages/_app.js
@@ -0,0 +1,14 @@
+
+import "../styles/globals.css";
+import Sidebar from "../components/sidebar"
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <Sidebar>
+    
+      <Component {...pageProps} />
+    </Sidebar>
+  );
+}
+
+export default MyApp;
